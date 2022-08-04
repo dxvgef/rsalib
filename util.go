2dd308571c13f0372db09a39b241ef82827227e8
dxvgef <dxvgef@outlook.com>
2022-08-04 16:22:31 +0800

    使用新的逻辑生成PEM，移除了旧逻辑依赖的函数

--- util.go
+++ util.go
@@ -1,30 +1,14 @@
 package rsalib
 
 import (
-	"bytes"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/pem"
+	"errors"
 	"strings"
 	"unsafe"
-)
-
-const (
-	publicKeyPrefix = "-----BEGIN PUBLIC KEY-----"
-	publicKeySuffix = "-----END PUBLIC KEY-----"
-
-	PKCS1Prefix = "-----BEGIN RSA PRIVATE KEY-----"
-	PKCS1Suffix = "-----END RSA PRIVATE KEY-----"
-
-	PKCS8Prefix = "-----BEGIN PRIVATE KEY-----"
-	PKCS8Suffix = "-----END PRIVATE KEY-----"
-)
-
-// 加密标准的版本
-type PKCSVersion uint8
-
-const (
-	PKCS1 PKCSVersion = 1
-	PKCS8 PKCSVersion = 8
 )
 
 // []byte转string
@@ -73,64 +57,92 @@ func HexDecode(src []byte) ([]byte, error) {
 	return dst, nil
 }
 
-// 格式化公钥
-func FormatPublicKey(key string) []byte {
-	return formatKey(key, publicKeyPrefix, publicKeySuffix, 64)
-}
-
-// 格式化私钥
-func FormatPKCSPrivateKey(key string, version PKCSVersion) []byte {
-	switch version {
-	case PKCS1:
-		return formatKey(key, PKCS1Prefix, PKCS1Suffix, 64)
-	case PKCS8:
-		return formatKey(key, PKCS8Prefix, PKCS8Suffix, 64)
-	}
-	return nil
-}
-
-func formatKey(raw, prefix, suffix string, lineCount int) []byte {
-	var err error
-	raw = strings.Replace(raw, PKCS8Prefix, "", 1)
-	raw = strings.Replace(raw, PKCS8Suffix, "", 1)
-	if raw == "" {
-		return nil
-	}
-	raw = strings.Replace(raw, prefix, "", 1)
-	raw = strings.Replace(raw, suffix, "", 1)
-	raw = strings.ReplaceAll(raw, " ", "")
-	raw = strings.ReplaceAll(raw, "\n", "")
-	raw = strings.ReplaceAll(raw, "\r", "")
-	raw = strings.ReplaceAll(raw, "\t", "")
-
-	var sl = len(raw)
-	var c = sl / lineCount
-	if sl%lineCount > 0 {
-		c++
-	}
-
-	var buf bytes.Buffer
-	if _, err = buf.WriteString(prefix + "\n"); err != nil {
-		return nil
-	}
-	for i := 0; i < c; i++ {
-		var b = i * lineCount
-		var e = b + lineCount
-		if e > sl {
-			if _, err = buf.WriteString(raw[b:]); err != nil {
-				return nil
+// ParsePEMBlocks 解析PEM区块
+func ParsePEMBlocks(data []byte) []*pem.Block {
+	var (
+		blocks []*pem.Block
+		block  *pem.Block
+		rest   []byte
+	)
+	block, rest = pem.Decode(data)
+	if block != nil {
+		blocks = append(blocks, block)
+		for len(rest) > 0 {
+			block, rest = pem.Decode(rest)
+			if block != nil {
+				blocks = append(blocks, block)
 			}
+		}
+	}
+	return blocks
+}
+
+// 解析RSA私钥，自动识别PKCS1和PKCS8
+func ParseRSAPrivateKey(data []byte) (privateKey *rsa.PrivateKey, version uint8, err error) {
+	version = 1
+	// 尝试PKCS1
+	privateKey, err = ParsePKCS1PrivateKey(data)
+	if err != nil {
+		// 如果PKCS1和PKCS8，直认为无效
+		if !strings.Contains(err.Error(), "PKCS8") {
+			return
+		}
+		// 尝试PKCS8
+		privateKey, err = ParsePKCS8PrivateKey(data)
+		if err != nil {
+			return
+		}
+		version = 8
+	}
+	return
+}
+
+// 解析PKCS1私钥
+func ParsePKCS1PrivateKey(data []byte) (*rsa.PrivateKey, error) {
+	return x509.ParsePKCS1PrivateKey(data)
+}
+
+// 解析PKCS8私钥
+func ParsePKCS8PrivateKey(data []byte) (privateKey *rsa.PrivateKey, err error) {
+	var (
+		parsedKey interface{}
+		ok        bool
+	)
+	parsedKey, err = x509.ParsePKCS8PrivateKey(data)
+	if err != nil {
+		return
+	}
+	privateKey, ok = parsedKey.(*rsa.PrivateKey)
+	if !ok {
+		err = errors.New("不是有效的RSA私钥")
+		return
+	}
+	return
+}
+
+// ParseRSAPublicKey 解析RSA公钥
+func ParseRSAPublicKey(data []byte) (publicKey *rsa.PublicKey, err error) {
+	var (
+		parsedKey interface{}
+		ok        bool
+		cert      *x509.Certificate
+	)
+
+	parsedKey, err = x509.ParsePKIXPublicKey(data)
+	if err != nil {
+		cert, err = x509.ParseCertificate(data)
+		if err == nil {
+			parsedKey = cert.PublicKey
 		} else {
-			if _, err = buf.WriteString(raw[b:e]); err != nil {
-				return nil
-			}
-		}
-		if _, err = buf.WriteString("\n"); err != nil {
-			return nil
+			return
 		}
 	}
-	if _, err = buf.WriteString(suffix); err != nil {
-		return nil
+
+	publicKey, ok = parsedKey.(*rsa.PublicKey)
+	if !ok {
+		err = errors.New("不是有效的RSA公钥")
+		return
 	}
-	return buf.Bytes()
+
+	return
 }
